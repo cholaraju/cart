a4550d750e2e996711bd36a6c3c45608fd0c8a20
Zedgamer9128 <zeeshantechnical9128@gmail.com>
2023-10-24 17:55:19 +0000

    Minor Spelling Fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,9 @@ import Products from "../components/Products";
 function Home() {
   return (
     <div>
-      <h2 className="heading">welcome to the Redux toolkit store</h2>
+      <h2 className="heading">Welcome to the Redux toolkit store</h2>
       <section>
-        <h3>products</h3>
+        <h3>Products</h3>
         <Products />
       </section>
     </div>
