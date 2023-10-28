4507ac6b56e0d3d563d33e349ee22cfe27da4a30
cholaraju <rajunelui@gmail.com>
2023-10-28 12:28:21 +0530

    changed import file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
+ import React from "react";
+ import { Link } from "react-router-dom";
 
-const Navbar = () => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <span className="logo"> VIJAY SHANKER CLOTH STORE</span>
-      <div>
-        <Link className="navLink" to="/">
-          Home
-        </Link>
-        <Link className="navLink" to="/Cart">
-          Cart
-        </Link>
-        <span className="cartCount">Cart Items: 0</span>
-      </div>
-    </div>
-  );
-};
+ const Navbar = () => {
+   return (
+     <div
+       style={{
+         display: "flex",
+         alignItems: "center",
+         justifyContent: "space-between",
+       }}
+     >
+       <span className="logo"> VIJAY SHANKER CLOTH STORE</span>
+       <div>
+         <Link className="navLink" to="/">
+           Home
+         </Link>
+         <Link className="navLink" to="/Cart">
+           Cart
+         </Link>
+         <span className="cartCount">Cart Items: 0</span>
+       </div>
+     </div>
+   );
+ };
 
-export default Navbar;
+ export default Navbar;
